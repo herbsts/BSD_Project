543aa6231f7d9adf26fd082020914955013e1038
Martin <martin.sonnberger@live.at>
2017-09-22 10:17:29 +0200

    add android app

---
+++ Boost_It/app/src/main/java/bsd/school2017/boost_it/MainActivity.java
@@ -0,0 +1,13 @@
+package bsd.school2017.boost_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
