38e8dc2d1ec8c31522d4b057d92d76a0796179bc
Pascal <laggerpas@gmail.com>
2017-10-27 11:23:24 +0200

    adapted ER

--- db/creates.sql
+++ db/creates.sql
@@ -1,5 +1,6 @@
 DROP TABLE Requests CASCADE CONSTRAINTS;
 DROP TABLE Users CASCADE CONSTRAINTS;
+DROP TABLE belongsTo CASCADE CONSTRAINTS;
 DROP TABLE Units CASCADE CONSTRAINTS;
 DROP TABLE Words CASCADE CONSTRAINTS;
 DROP TABLE Phrases CASCADE CONSTRAINTS;
@@ -25,6 +26,15 @@ CREATE TABLE Users(
 	username 	VARCHAR2(30),
 	password	VARCHAR2(15),
 	role		INTEGER,
+);
+
+CREATE TABLE belongsTo(
+	user_id 	INTEGER,
+	teacher_id 	INTEGER,
+	
+	CONSTRAINT pk_BelongsTo PRIMARY KEY (user_id, teacher_id),
+	CONSTRAINT fk_BelongsToUser FOREIGN KEY (user_id) REFERENCES Users(user_id),
+	CONSTRAINT fk_BelongsToTeacher FOREIGN KEY (teacher_id) REFERENCES Users(user_id),
 );
 
 CREATE TABLE Units(
@@ -98,6 +108,3 @@ CREATE TABLE phraseBelongs(
 	CONSTRAINT fk_PhraseBelongsPhrase FOREIGN KEY (phrase_id) REFERENCES Phrases(phrase_id),
 	CONSTRAINT fk_PhraseBelongsUnit FOREIGN KEY (unit_id) REFERENCES Unit(unit_id)
 );
-
-
-
