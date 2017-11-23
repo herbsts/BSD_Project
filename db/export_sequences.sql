e83b83ff0f79361d3cda8775c708eb1e60c03250
Pascal <laggerpas@gmail.com>
2017-11-23 21:18:39 +0100

    added folder for web version; db exports

---
+++ db/export_sequences.sql
@@ -0,0 +1,33 @@
+--------------------------------------------------------
+--  Datei erstellt -Donnerstag-November-23-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence SEQ_OWNSPHRASE
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_OWNSPHRASE"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 3 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_WORDS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_WORDS"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_USERID
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_USERID"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_UNITS
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_UNITS"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_PHRASES
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_PHRASES"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SEQ_OWNSWORD
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "D5B12"."SEQ_OWNSWORD"  MINVALUE 0 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
